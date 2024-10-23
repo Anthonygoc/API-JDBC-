3dc307c671f5d3b26f06562a461bc9c867fbb513
AnthonyGoc <anthonygabrieloliveiracruz@gmail.com>
2024-10-22 21:58:09 -0400

    commit

--- src/main/java/ifmt/cba/apps/excluir1.java
+++ src/main/java/ifmt/cba/apps/excluir1.java
@@ -1,5 +1,40 @@
 package ifmt.cba.apps;
 
-public class Alterar2 {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+
+import ifmt.cba.util.FabricaConexao;
+
+public class excluir1 {
+
+ public static void main(String[] args) {
+     
+    Connection conexao = null;
+    
+    PreparedStatement comando = null;
+
+    int codigo = Integer.parseInt(JOptionPane.showInputDialog("Forneca o codigo do grupo de produto a ser excluido "));
+
+    try{ 
+        conexao = FabricaConexao.obterConexao();
+        comando = conexao.prepareStatement("DELETE FROM grupoproduto WHERE codigo =?");
+        comando.setInt(1, codigo);
+
+        int contRec = comando.executeUpdate();
+        System.out.println("Exclusao realizada com sucesso ["+contRec + "Excluido]");
+    }catch(Exception ex){
+        System.out.println("Erro ao excluir o grupo de produto " + ex.toString());
+    }finally{
+        try{
+            comando.close();
+            conexao.close();
+        }catch (SQLException ex){
+            System.out.println("Erro ao desconectar " + ex.toString());
+        }
+    }
+
+ }
 
 }
